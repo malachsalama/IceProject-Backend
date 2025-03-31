6646ccb60c22076cf2f79adab57290f9ed52f0e6
Malach <malach.salama@gmail.com>
2025-04-01 01:23:01 +0300

    configure Swagger UI to display the authorization button

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
     .setTitle('IceProject API Documentation')
     .setDescription('API documentation for IceProject (POS/ERP system)')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
